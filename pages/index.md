73091c2ec057c24f5b9e015aa805c281b9b095d5
xijinping0 <111333464+xijinping0@users.noreply.github.com>
2024-05-20 23:07:14 -0700

    complete nextra setup

---
+++ pages/index.md
@@ -0,0 +1,4 @@
+# 目录
+
+- [习近平和他的情人们](/lovers/toc.md)
+- [红色赌盘](/roulette/cover.md)
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,68 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig, useConfig} from 'nextra-theme-docs'
+import { useRouter } from "next/router";
+
+const shortFormatter = new Intl.DateTimeFormat('zh-CN', {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+});
+
+const getBookName = (path: string): string | undefined => {
+  const tokens = path.split('/');
+  if (tokens.length < 0) {
+    return undefined;
+  }
+  switch (tokens[1]) {
+    case 'lovers': {
+      return '习近平和他的情人们';
+    }
+    case 'roulette': {
+      return '红色赌盘';
+    }
+    default: {
+      return undefined;
+    }
+  }
+}
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>天朝禁书</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/xijinping0/books',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: 'https://github.com/xijinping0/books',
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="天朝禁书" />
+      <meta property="og:description" content="天朝禁书" />
+    </>
+  ),
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    return {
+      titleTemplate: ['%s', getBookName(asPath)].filter((s) => Boolean).join(' | ')
+    }
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  toc: {
+    backToTop: true,
+  },
+  editLink: {
+    text: '在 GitHub 上编辑此页',
+  },
+  gitTimestamp({ timestamp }) {
+    return (
+      <div>
+        更新时间：{shortFormatter.format(new Date(timestamp))}
+      </div>
+    )
+  },
+  feedback: {
+    content: null,
+  },
   footer: {
-    text: 'Nextra Docs Template',
+    text: '天朝禁书',
   },
 }
 
