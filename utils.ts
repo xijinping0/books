7c3224bd30911a661e7fc8094e537b5065a6f6f4
习近平 <111333464+xijinping0@users.noreply.github.com>
2024-06-12 23:04:44 -0700

    add red sun (#6)

--- utils.ts
+++ utils.ts
@@ -1,5 +1,6 @@
 export const BOOK_NAMES: Record<string, string> = {
   'lovers': '习近平和他的情人们',
+  'red-sun': '红太阳是怎样升起的',
   'roulette': '红色赌盘',
   'tombstone': '墓碑',
   'private-life-of-mao': '毛泽东私人医生回忆录',
