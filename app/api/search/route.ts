bfb9399cf88f92bf887224e30514547e3e9d99ee
xijinping0 <111333464+xijinping0@users.noreply.github.com>
2025-06-14 23:55:48 -0700

    修复搜索
    https://github.com/fuma-nama/fumadocs/issues/1804#issuecomment-2847641251

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,15 +3,11 @@ import { createFromSource } from 'fumadocs-core/search/server';
 import { createTokenizer } from '@orama/tokenizers/mandarin';
 
 export const { GET } = createFromSource(source, {
-  localeMap: {
-    cn: {
-      components: {
-        tokenizer: createTokenizer(),
-      },
-      search: {
-        threshold: 0,
-        tolerance: 0,
-      },
-    },
+  components: {
+    tokenizer: createTokenizer(),
+  },
+  search: {
+    threshold: 0,
+    tolerance: 0,
   },
 });
