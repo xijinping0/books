f60a8f59b502c579f6ac2f5f09bab8a418e83794
xijinping0 <111333464+xijinping0@users.noreply.github.com>
2025-09-18 14:26:31 -0700

    add query debounce

--- components/search.tsx
+++ components/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useCallback, useEffect, useState } from 'react';
 import { useDocsSearch } from 'fumadocs-core/search/client';
 import {
   SearchDialog,
@@ -16,13 +17,31 @@ import { useI18n } from 'fumadocs-ui/contexts/i18n';
 
 export default function CustomSearchDialog(props: SharedProps) {
   const { locale } = useI18n();
-  const { search, setSearch, query } = useDocsSearch({
+  const [localSearch, setLocalSearch] = useState('');
+  const { setSearch, query } = useDocsSearch({
     type: 'fetch',
     locale,
   });
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearch(localSearch);
+    }, 700);
+
+    return () => clearTimeout(timer);
+  }, [localSearch, setSearch]);
+
+  const handleSearchChange = useCallback((value: string) => {
+    setLocalSearch(value);
+  }, []);
+
   return (
-    <SearchDialog search={search} onSearchChange={setSearch} isLoading={query.isLoading} {...props}>
+    <SearchDialog
+      search={localSearch}
+      onSearchChange={handleSearchChange}
+      isLoading={query.isLoading}
+      {...props}
+    >
       <SearchDialogOverlay />
       <SearchDialogContent>
         <SearchDialogHeader>
