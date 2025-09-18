664316318c65813f41ab036633db7db564ef8040
xijinping0 <111333464+xijinping0@users.noreply.github.com>
2025-09-18 14:15:37 -0700

    Custom search component

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,13 +1,18 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 import { createTokenizer } from '@orama/tokenizers/mandarin';
+import { stopwords } from '@orama/stopwords/mandarin';
 
 export const { GET } = createFromSource(source, {
   components: {
-    tokenizer: createTokenizer(),
+    tokenizer: createTokenizer({
+      language: 'mandarin',
+      stopWords: stopwords,
+    }),
   },
   search: {
     threshold: 0,
-    tolerance: 0,
+    exact: true,
+    limit: 10,
   },
 });
